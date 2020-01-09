8570d8f7034d964d36857840c1a1c41673c62d5f
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2020-01-09 23:09:58 +0200

    Completed meme generator

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,13 @@ import Face from '../images/face.png'
 
 function Header() {
     return (
-        <div>
-            <img src={Face} className='logo' width='300px' height='250px' alt='where?' />
-            <h1>Hi, I am a header for Meme Generator</h1>
-        </div>
+        <header>
+            <img 
+            src={Face} 
+            className='logo' 
+            alt='where?' />
+            <p>Hi, I am a header for Meme Generator</p>
+        </header>
         
     )
 }
--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-
 class MemeGenerator extends React.Component {
     constructor() {
         super()
         this.state = {
+            loading: false,
             topText: '',
             bottomText: '',
-            randomImg: 'http://i.imgflip.com/lbij.jpg',
+            randomImg: 'http://i.imgflip.com/1bij.jpg',
             allMemes: []
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.randomClick = this.randomClick.bind(this);
     }
 
     componentDidMount() {
@@ -17,17 +19,50 @@ class MemeGenerator extends React.Component {
             .then(res => res.json())
             .then(res => {
                 const {memes} = res.data
-                console.log(memes[0])
                 this.setState({allMemes: memes})
             })
     }
+    
+    handleChange(event) {
+        const {name, value} = event.target;
+        this.setState({
+           [name]: value
+        })
+    }
+
+    randomClick(event) {
+        event.preventDefault();
+        const randomNum = Math.floor(Math.random() * this.state.allMemes.length);
+        const randomNameImg = this.state.allMemes[randomNum].url;
+        this.setState({ randomImg: randomNameImg})
+     }
+
 
     render() {
         return (
-            <div>Something to return!
-                <p>{this.state.topText}</p>  
-                <p>{this.state.bottomText}</p>  
-                <p>{this.state.randomImg}</p>  
+            <div>
+                <form className='meme-form' onSubmit={this.randomClick}>
+                    <input
+                    type='text'
+                    name='topText'
+                    placeholder='Top text'
+                    value={this.state.topText}
+                    onChange={this.handleChange}
+                    /> 
+                    <input
+                    type='text'
+                    name='bottomText'
+                    placeholder='Bottom Text'
+                    value={this.state.bottomText}
+                    onChange={this.handleChange}
+                    />
+                    <button>Gen</button>
+                </form>
+                <div className='meme'>
+                    <img src={this.state.randomImg} alt='none_img' />
+                    <h2 className='top'>{this.state.topText}</h2>
+                    <h2 className='bottom'>{this.state.bottomText}</h2>
+                </div>
             </div>
         ) 
     }
